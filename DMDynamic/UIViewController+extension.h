4f4a9c83395471c03241f8ee2bfda4637c19da66
Derek Lix <iderek09@gmail.com>
2015-01-05 11:15:30 +0800

    add UIViewController extension
    add UIViewController extension

---
+++ DMDynamic/UIViewController+extension.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+extension.h
+//  DMDynamic
+//
+//  Created by Derek Lix on 15/1/5.
+//  Copyright (c) 2015年 Derek Lix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIViewController (extension)
+
+@end
---
+++ DMDynamic/UIViewController+extension.m
@@ -0,0 +1,56 @@
+//
+//  UIViewController+extension.m
+//  DMDynamic
+//
+//  Created by Derek Lix on 15/1/5.
+//  Copyright (c) 2015年 Derek Lix. All rights reserved.
+//
+
+#import "UIViewController+extension.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (extension)
+
+
++ (void)load {
+    NSString *className = NSStringFromClass(self.class);
+    NSLog(@"classname %@", className);
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        // When swizzling a class method, use the following:
+        // Class class = object_getClass((id)self);
+        
+        SEL originalSelector = @selector(viewWillAppear:);
+        SEL swizzledSelector = @selector(xxx_viewWillAppear:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+#pragma mark - Method Swizzling
+
+- (void)xxx_viewWillAppear:(BOOL)animated {
+    [self xxx_viewWillAppear:animated];
+    NSLog(@"viewWillAppear: %@", self);
+}
+
+
+@end
